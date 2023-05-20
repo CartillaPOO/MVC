0694c221aafc9456a4ed80c9f7c2176a08a3f81e
Alvoid101 <10101010alvoid@gmail.com>
2023-05-20 11:09:23 -0500

    Agrgando paquete cvo

---
+++ 14.AreaYPerimetroCirculo/ExplicacionMVC/cvo/modelo/Colores.java
@@ -0,0 +1,19 @@
+package cvo.modelo;
+
+import java.awt.Color;
+
+public class Colores {
+    //---Atributos---
+
+    //Colores para las lineas
+    public static Color ClASE = new Color(229, 56, 38);
+    public static Color OBJETO = new Color(235, 219, 178);
+    public static Color INSTANCIA = new Color(250, 186, 43);
+
+    public static Color RESALTADO = Color.WHITE;
+    public static Color FONDO = new Color(29, 32, 33);
+
+    //Colores para los botones
+    public static Color FONDO_BOTON = new Color(31, 32, 41);
+    public static Color FUENTE_BOTON = new Color(235, 219, 178);
+}
