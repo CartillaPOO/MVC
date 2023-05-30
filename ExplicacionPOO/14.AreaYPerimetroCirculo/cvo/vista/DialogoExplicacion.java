7bd13118afde6bf2c43dee40e56692dd3ee4edb1
Alvoid101 <10101010alvoid@gmail.com>
2023-05-30 11:43:50 -0500

    Mover ventanas

--- ExplicacionPOO/14.AreaYPerimetroCirculo/cvo/vista/DialogoExplicacion.java
+++ ExplicacionPOO/14.AreaYPerimetroCirculo/cvo/vista/DialogoExplicacion.java
@@ -1,11 +1,17 @@
 package cvo.vista;
 import java.awt.Color;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 import java.util.Timer;
 
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JLayeredPane;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,11 +24,19 @@ public class DialogoExplicacion extends JDialog{
     private int posY;
     private int alto;
     private int ancho;
+    private JLayeredPane capa;
     
     private String rutaImagen = "cvo/Recursos/Diagramas/";
     private String rutaGif;
     private ImageIcon icon; 
     private JLabel label;
+        
+    private Point initialClick;
+
+    private JLabel lbCerrar;
+
+
+
 
     
     public DialogoExplicacion(String imagen){
@@ -38,7 +52,8 @@ public class DialogoExplicacion extends JDialog{
         label.setBounds(0, 0, ancho, alto);
         this.add(label);
         
-        // Caracteristicas del diálogo 
+        // Caracteristicas del diálogo
+        moverVentana();
         this.setUndecorated(true);
         this.setTitle("Objetos en RAM");
         this.setSize(ancho, alto);
@@ -59,14 +74,25 @@ public class DialogoExplicacion extends JDialog{
         
         ancho = icon.getIconWidth();
         alto = icon.getIconHeight();
+        
+        this.capa = new JLayeredPane();
+        capa.setBounds(0, 0, ancho, alto);
 
         label = new JLabel(icon);
         label.setBounds(0, 0, ancho, alto);
-        this.add(label);
+        capa.add(label, JLayeredPane.DEFAULT_LAYER);
 
+        lbCerrar = new JLabel("Cerrar");
+        lbCerrar.setBounds(0, 0, 100, 30);
+        lbCerrar.setForeground(Color.WHITE);
+        capa.add(lbCerrar, JLayeredPane.PALETTE_LAYER);
+
+        this.add(capa);
+        
         desactivarGif(rutaImagen, tiempo);
         
         // Caracteristicas del diálogo 
+        moverVentana();
         this.setUndecorated(true);
         this.setTitle("Objetos en RAM");
         this.setSize(ancho, alto);
@@ -85,6 +111,25 @@ public class DialogoExplicacion extends JDialog{
                 label.setIcon(icon);
             }
         }, tiempo * 1000);
+    }
+
+
+    public void moverVentana(){
+        addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                initialClick = e.getPoint(); // Guarda la posición inicial del click
+            }
+        });
+
+        // Agrega un MouseMotionListener para detectar eventos de movimiento del mouse
+        addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                // Calcula la nueva ubicación de la ventana al arrastrar el mouse
+                int x = getLocation().x + e.getX() - initialClick.x;
+                int y = getLocation().y + e.getY() - initialClick.y;
+                setLocation(x, y); // Establece la nueva ubicación de la ventana
+            }
+        });
     }
 
     
