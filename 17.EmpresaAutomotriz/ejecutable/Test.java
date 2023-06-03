f8333f2eef649e6abcd162ecf007b4fc3fd8ae8e
Alvoid101 <10101010alvoid@gmail.com>
2023-06-03 11:32:07 -0500

    corrgiendo valor nomina

--- 17.EmpresaAutomotriz/ejecutable/Test.java
+++ 17.EmpresaAutomotriz/ejecutable/Test.java
@@ -8,8 +8,8 @@ public class Test
 {
     public static void main(String[] args) 
     {
-        EmpresaAutomotriz empresa = new EmpresaAutomotriz();
-        VentanaPrincipal venPrin = new VentanaPrincipal();
-        Controlador controlador = new Controlador(venPrin, empresa);
+        VentanaPrincipal miVentana = new VentanaPrincipal();
+        EmpresaAutomotriz miEmpresa = new EmpresaAutomotriz();
+        Controlador controlador = new Controlador(miVentana, miEmpresa);
     }
 }
