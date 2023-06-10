0b754241b0f1c20767bb0e553de63e4a87d3ae1a
Alvoid101 <10101010alvoid@gmail.com>
2023-06-10 10:30:42 -0500

    Agregando sistemacvo

---
+++ ExplicacionPOO/17.EmpresaAutomotriz/Tareas.java
@@ -0,0 +1,3 @@
+public class Tareas {
+    //TODO: Hacer que el tamaño del DialogoExpliacion se adapte al tamaño de la imagen
+}
---
+++ ExplicacionPOO/17.EmpresaAutomotriz/ejecutable/test.java
@@ -0,0 +1,13 @@
+package ejecutable;
+
+import cvo.eventos.EventosBoton;
+import cvo.eventos.EventosMouse;
+import cvo.vista.FrameTest;
+
+public class test {
+    public static void main(String[] args) {
+       FrameTest miTest = new FrameTest("EmpresaAutomotriz", 15);
+       EventosMouse eventosMouse = new EventosMouse(miTest);
+       EventosBoton eventosBoton = new EventosBoton(miTest);
+    }
+}
